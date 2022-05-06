d5da3df99f61afffc58be93eb129bb0e4dc58513
kenyansa <kenyansajustine@gmail.com>
2022-05-06 09:51:31 +0300

    completed assignment

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+//destructured, with default URL source
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
 }) {
+  //from here, no prop.whatever stuff will be needed
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
